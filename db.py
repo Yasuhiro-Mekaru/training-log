7d83e5935265383c28aec7ffb4e54055bd295736
Yasuhiro-Mekaru <taijyu720@yahoo.co.jp>
2020-07-26 14:58:23 +0900

    eighth

--- db.py
+++ db.py
@@ -16,8 +16,10 @@ SSL_KEY = '/app/unlock-key.pem'
 class My_log_database(object):
 
 	def insert_data(self):
+		# conn = mysql.connector.connect(host=DB_HOST, user=DB_USERNAME, password=DB_PASSWORD, database=DB_DATABASE, 
+		# 	ssl_ca=SSL_CA, ssl_cert=SSL_CERT, ssl_key=SSL_KEY)
 		conn = mysql.connector.connect(host=DB_HOST, user=DB_USERNAME, password=DB_PASSWORD, database=DB_DATABASE, 
-			ssl_ca=SSL_CA, ssl_cert=SSL_CERT, ssl_key=SSL_KEY)
+			ssl_disabled=True)
 		# cursol = conn.cursol()
 		# cursol.execute('INSERT INTO weather_select values(1, "晴れ: 追い風")')
 		# conn.commit()
