30185e962ba4273c1aeaf24d15055605e996796a
Yasuhiro-Mekaru <taijyu720@yahoo.co.jp>
2020-08-01 10:46:40 +0900

    testflask11

--- db.py
+++ db.py
@@ -18,9 +18,8 @@ class My_log_database(object):
 	def insert_data(self):
 		# conn = mysql.connector.connect(host=DB_HOST, user=DB_USERNAME, password=DB_PASSWORD, database=DB_DATABASE, 
 		# 	ssl_ca=SSL_CA, ssl_cert=SSL_CERT, ssl_key=SSL_KEY, ssl_verify_cert=True)
-		# conn = mysql.connector.connect(host=DB_HOST, user=DB_USERNAME, password=DB_PASSWORD, database=DB_DATABASE, 
-		# 	ssl_disabled=True)
-		conn = mysql.connector.connect(host=DB_HOST, user=DB_USERNAME, password=DB_PASSWORD, database=DB_DATABASE)
+		conn = mysql.connector.connect(host=DB_HOST, user=DB_USERNAME, password=DB_PASSWORD, database=DB_DATABASE, 
+			ssl_disabled=True)
 		# re = conn.is_connected()
 		# cursor = conn.cursor()
 		# # cursor.execute('INSERT INTO test values(2, 200)')
