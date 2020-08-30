140808612f9bf8e26186f32383571366c3490111
Yasuhiro-Mekaru <taijyu720@yahoo.co.jp>
2020-08-30 11:48:14 +0900

    add logs

--- date.py
+++ date.py
@@ -56,18 +56,22 @@ class My_date(object):
 				changed_datas.append(data)
 				logger.info({
 					'action': 'date.py',
-					'for statement changed_datas': changed_datas
+					'for statement 1 if: changed_datas': changed_datas
 					})
 			else:
 				for changed_data in changed_datas:
 					if data[0] == changed_data[0]:
 						changed_data[1] = data[1] + changed_data[1]
 						changed_data[2] = data[2] + changed_data[2]
+						logger.info({
+							'action': 'date.py',
+							'for statement 2 if: changed_datas': changed_datas
+							})
 					else:
 						changed_datas.append(data)
 						logger.info({
 							'action': 'date.py',
-							'for statement changed_datas': changed_datas
+							'for statement 2 else: changed_datas': changed_datas
 							})
 
 		logger.info({
