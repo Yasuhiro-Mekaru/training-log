e9b25d63871af57880b464155e16fec0467a846a
Yasuhiro-Mekaru <taijyu720@yahoo.co.jp>
2020-09-07 06:44:24 +0900

    a

--- bokeh_chart.py
+++ bokeh_chart.py
@@ -33,7 +33,7 @@ class My_bokeh_chart(object):
 	    tools = "pan,box_zoom,reset,save"
 
 	    # チャート内のツールチップのツールチップを設定
-        milage_hover_tool = HoverTool(
+	    milage_hover_tool = HoverTool(
 	    		tooltips=[
 		    		('日付', "@index{%F}"),
 		    		('当日走行距離', '@Milage'+' (km)'),
